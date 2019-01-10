d3bd871335ff89c949236d3e7a9fe89aa9d9c6d1
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2019-01-10 17:58:56 -0200

    Blog status and toogle option added

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   resources :portfolios
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
-  resources :blogs
+  resources :blogs do
+    member do
+      post :toogle_status
+    end
+  end
 
   root to: 'pages#home'
 end
