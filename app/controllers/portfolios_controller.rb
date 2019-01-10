c5c4ee88c380c47daf5cf191231a8f13a43c8027
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2019-01-10 14:04:00 -0200

    Edit and update functionalities

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -12,12 +12,29 @@ class PortfoliosController < ApplicationController
     
         respond_to do |format|
           if @portfolio.save
-            format.html { redirect_to portfolios_path    , notice: 'Portfolio was successfully created.' }
+            format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
             format.json { render :show, status: :created, location: @portfolio }
           else
             format.html { render :new }
             format.json { render json: @portfolio.errors, status: :unprocessable_entity }
           end
         end
+    end
+
+    def edit
+      @portfolio = Portfolio.find(params[:id])
+    end
+
+    def update
+      @portfolio = Portfolio.find(params[:id])
+      respond_to do |format|
+        if @portfolio.update(params.require(:portfolio).permit(:title, :subtitle,:body))
+          format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
+          format.json { render :show, status: :ok, location: @portfolio }
+        else
+          format.html { render :edit }
+          format.json { render json: @portfolio.errors, status: :unprocessable_entity }
+        end
       end
+    end
 end
