f3bb0330cc035596c26c881d4e1e40b0890139ac
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2019-01-21 17:30:30 -0200

    Integrated nested attributes for portfolio

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,7 +1,8 @@
 class Portfolio < ApplicationRecord
-    include Placeholder
-
     has_many :technologies
+    accepts_nested_attributes_for :technologies, reject_if: lambda { |attr| attr['name'].blank? } # Do not accepts if name is empty
+    
+    include Placeholder
     validates_presence_of :title, :body, :main_image, :thumb_image
     after_initialize :set_defaults
 
