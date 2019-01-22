ff946d878817005560e8981d3ee63d078f5223dc
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2019-01-22 18:15:25 -0200

    Refactored strong params for portfolio controller

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,57 +1,70 @@
 class PortfoliosController < ApplicationController
-    def index
-        @portfolios = Portfolio.all
-    end
+  def index
+      @portfolios = Portfolio.all
+  end
 
-    def angular
-      @angular_portfolios = Portfolio.angular
-    end
+  def angular
+    @angular_portfolios = Portfolio.angular
+  end
 
-    def new
-      @portfolio = Portfolio.new
-      3.times { @portfolio.technologies.build }
-    end
+  def new
+    @portfolio = Portfolio.new
+    3.times { @portfolio.technologies.build }
+  end
 
-    def create
-      @portfolio = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle,:body,
-                                                                     technologies_attributes: [:name]))
-    
-      respond_to do |format|
-        if @portfolio.save
-          format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
-        else
-          format.html { render :new }
-        end
+  def create
+    @portfolio = Portfolio.new(portfolio_params)
+
+    respond_to do |format|
+      if @portfolio.save
+        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
+      else
+        format.html { render :new }
       end
     end
+  end
 
-    def edit
-      @portfolio = Portfolio.find(params[:id])
-    end
+  def edit
+    @portfolio = Portfolio.find(params[:id])
+  end
 
-    def update
-      @portfolio = Portfolio.find(params[:id])
-      respond_to do |format|
-        if @portfolio.update(params.require(:portfolio).permit(:title, :subtitle,:body))
-          format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
-        else
-          format.html { render :edit }
-        end
+  def update
+    @portfolio = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio.update(portfolio_params)
+        format.html {
+          redirect_to portfolios_path,
+          notice: 'Portfolio was successfully updated.',
+        }
+      else
+        format.html { render :edit }
       end
     end
+  end
 
-    def show
-      @portfolio = Portfolio.find(params[:id])
+  def show
+    @portfolio = Portfolio.find(params[:id])
+  end
+
+  def destroy
+    @portfolio = Portfolio.find(params[:id])
+
+    @portfolio.destroy
+
+    # Redirect
+    respond_to do |format|
+      format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully destroyed.' }
     end
+  end
 
-    def destroy
-      @portfolio = Portfolio.find(params[:id])
+  private
 
-      @portfolio.destroy
-
-      # Redirect
-      respond_to do |format|
-        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully destroyed.' }
-      end
-    end
+  def portfolio_params
+    params.require(:portfolio).permit(
+      :title,
+      :subtitle,
+      :body,
+      technologies_attributes: [:name]
+    )
+  end
 end
