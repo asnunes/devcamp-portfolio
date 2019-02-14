f199da5762733e8bdecf465f81a4c90c75cc9080
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2019-02-14 10:08:37 -0200

    Updated Home page layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
-  def login_helper
+  def login_helper(class_name = "")
     if current_user.is_a?(GuestUser)
-      (link_to "Register",  new_user_registration_path, method: :get) +
-      "<br>".html_safe +
-      (link_to "Login",  new_user_session_path, method: :get)
+      (link_to "Register",  new_user_registration_path, method: :get, class: class_name) +
+      " ".html_safe +
+      (link_to "Login",  new_user_session_path, method: :get, class: class_name)
     else
-      link_to "Logout", destroy_user_session_path, method: :delete
+      link_to "Logout", destroy_user_session_path, method: :delete, class: class_name
     end
   end
 
@@ -14,5 +14,9 @@ module ApplicationHelper
       content = "You came from #{session[:source]}..."
       content_tag :p, content, class: "source-tag"
     end
+  end
+
+  def copyright_generator
+    content_tag :p, "Alexandre Nunes | 2019 - MIT"
   end
 end
